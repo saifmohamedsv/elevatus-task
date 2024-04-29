36eb860c3fbdb954751aebbf17ccf88589875b88
saifmohamed-dev <155442820+saifmohamed-dev@users.noreply.github.com>
2024-04-29 19:52:07 +0300

    init components

---
+++ src/components/common/jobs-list.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Job } from "../../types/job";
+
+interface Props {
+  data: Job[];
+}
+
+export const JobsList: FC<Props> = ({}) => {
+  return <div> New page </div>;
+};
